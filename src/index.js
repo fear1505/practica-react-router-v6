22e196106443c44838a3501033446e0ab83ea1c5
fear1505 <rodilfelix16@gmail.com>
2023-01-12 21:26:41 -0300

    primer commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+
+import "bootstrap/dist/css/bootstrap.min.css"
+
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Inicio from './routers/Inicio';
+import Blog from './routers/Blog';
+import Post from './routers/Post';
+import Contacto from './routers/Contacto';
+import NoEncontrado from './routers/NoEncontrado';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<App />}>
+          <Route index element={<Inicio />} />
+          <Route path='blog' element={<Blog/>} />
+          <Route path='blog/:id' element={<Post />} />
+          <Route path='contacto' element={<Contacto/>} />
+
+          <Route path='*' element={<NoEncontrado />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
---
+++ src/routers/Blog.jsx
@@ -0,0 +1,53 @@
+import { useFetch } from "../hooks/useFetch"
+import { Link, useSearchParams } from "react-router-dom"
+
+const Blog = () => {
+  let [searchParams, setSearchParams] = useSearchParams()
+
+  const {data, error, loading} = useFetch("https://jsonplaceholder.typicode.com/posts")
+
+  if(loading){
+    return <h2>Loading...</h2>
+  }
+
+  if(error !== ""){
+    return <h2>{error}</h2>
+  }
+
+  const handleChanger = e => {
+    let filter = e.target.value
+    if(filter){
+      setSearchParams({filter})
+    }else{
+      setSearchParams({})
+    }
+  }
+
+  return (
+    <div>
+      <h1>Blog</h1>
+      <input 
+      type="text" 
+      className="form-control mb-2"
+      value={searchParams.get("filter") || ""}
+      onChange={handleChanger}
+      name = "filter"
+      />
+      {
+        data.filter((item)=>{
+          let filter = searchParams.get("filter")
+          if(!filter) return true
+
+          let title = item.title.toLowerCase()
+          return title.startsWith(filter.toLowerCase())
+        }).map((item) => (
+          <h4 key={item.id}>
+            <Link to={`/blog/${item.id}`}>{item.id} - {item.title}</Link>
+          </h4>
+        ))
+      }
+    </div>
+  )
+}
+
+export default Blog
